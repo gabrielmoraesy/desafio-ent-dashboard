dbec1485583c413dd0a89c44fa7d76d1454cfba3
gabrielmoraesy <ygabrielmoraes@gmail.com>
2025-01-23 19:31:54 -0300

    💄 Condicional show unit filter

--- src/pages/DashboardReviews/fragments/MainDashboardReviews/fragments/NpsUnitChart/NpsUnitChart.tsx
+++ src/pages/DashboardReviews/fragments/MainDashboardReviews/fragments/NpsUnitChart/NpsUnitChart.tsx
@@ -5,14 +5,14 @@ import { FilterX } from "lucide-react";
 import { useNpsUnitChart } from "./NpsUnitChart.hook";
 
 export const NpsUnitChart = () => {
-  const { setUnitSelected } = useFiltersContext();
+  const { unitSelected, setUnitSelected } = useFiltersContext();
   const { NpsUnitChartData } = useNpsUnitChart();
 
   return (
     <div className="bg-gray-100 dark:bg-gray-800 p-6 rounded-lg col-span-1 xl:col-span-2">
       <div className="flex justify-between">
         <h2 className="text-base sm:text-lg font-bold mb-4">NPS Por Unidade</h2>
-        <FilterX onClick={() => setUnitSelected("")} />
+        {unitSelected && <FilterX onClick={() => setUnitSelected("")} />}
       </div>
       <Chart
         options={NpsUnitChartData.options as unknown as ApexOptions}
